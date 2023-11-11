946b9a5e4b9aebe5ff3b592452d5991fc3e7f968
MohamedAmineALLAF <amine.allafi@gmail.com>
2023-11-11 08:24:14 +0100

    sc

---
+++ src/main/java/org/sid/bankaccountservice/service/AccountService.java
@@ -0,0 +1,8 @@
+package org.sid.bankaccountservice.service;
+
+import org.sid.bankaccountservice.dto.BankAccountReponseDTO;
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+
+public interface AccountService {
+    BankAccountReponseDTO addAccount(BankAccountRequestDTO bankAccountDTO);
+}
